071adf334a52499319ff5b947d3f8a2244e81bc7
Piawai Said Umbara <piawai.said@gmail.com>
2022-10-27 13:26:20 +0700

    Modify register page

--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+import styles from '../styles/Register.module.css'
 
 export default function Register() {
     return (
@@ -9,39 +10,37 @@ export default function Register() {
                 <meta name="description" content="Register page" />
             </Head>
 
-            <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-                <div className="bg-white flex w-2/3 max-w-6xl">
-                    <div className="bg-black w-3/5 p-5 text-white py-36 px-12">
-                        <div className="text-left font-bold ">
-                            Logo
-                        </div>
-                        <div className="py-10">
-
-                        </div>
-                        <h2 className="text-3xl font-bold mb-2">Lorem ipsum dolor sit</h2>
-                        <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec luctus lorem..</p>
-                    </div>
-                    <div className="w-2/5 text-black py-36 px-12">
-                        <h2 className="text-3xl font-bold mb-2">Register</h2>
-                        <a href="#" className="border-2 border-black py-2 px-12 font-semibold hover:bg-gray hover:text-white">Submit</a>
+            <main>
+                <div className={`${styles.split} ${styles.left}`}>
+                    <div className={styles.register_centered}>
+                        <h1>Fishmark</h1>
+                        <p>Buat akun dan dapatkan layanan spesialmu</p>
                     </div>
                 </div>
-
-                <div>
-                    <div>
-                        <form action="/send-data-here" method="post">
-                            <label for="email">E-mail:</label>
-                            <input type="text" id="email" name="email" />
-
-                            <label for="username">Username:</label>
-                            <input type="text" id="username" name="username" />
-
-                            <label for="password">Password:</label>
-                            <input type="password" id="password" name="password" />
-
-                            <label for="password">Confirm Password:</label>
-                            <input type="password" id="confirm" name="confirm" />
-                          
+                <div className={`${styles.split} ${styles.right}`}>
+                    <div className={styles.form_centered}>
+                        <form action="/send-data" method="post">
+                            <h1>Register</h1>
+                            <div className={styles.form_group}>
+                                <label for="email">E-mail: </label>
+                                <input type="email" id="email" name="email" />
+                            </div>
+                            <div className={styles.form_group}>
+                                <label for="username">Username: </label>
+                                <input type="text" id="username" name="username" />
+                            </div>
+                            <div className={styles.form_group}>
+                                <label for="password">Password: </label>
+                                <input type="password" id="password" name="password" />
+                            </div>
+                            <div className={styles.form_group}>
+                                <label for="confirm">Confirm Password: </label>
+                                <input type="password" id="confirm" name="confirm" />
+                            </div>
+                            <div className={styles.form_group}>
+                                <input type="checkbox" id="terms" name="terms" value="terms"/>
+                                <label for="terms"> Saya menerima segala syarat dan ketentuan</label>
+                            </div>
                             <button type="submit">Submit</button>
                         </form>
                     </div>
